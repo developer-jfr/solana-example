94ab90f8e877fb957aba6f313eeafcb171147981
frdevelopergen <erkinbaevbakai8@gmail.com>
2022-07-26 12:15:01 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css';
+import ConnectWallet from './components/ConnectWallet';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Solana examples</h1>
+      </header>
+      <ConnectWallet />
+    </div>
+  );
+}
+
+export default App;
